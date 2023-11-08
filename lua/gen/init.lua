77b6c7bc77957a29dd34ed4ffe3f15df9563f083
mrktix <mrktix@gmail.com>
2023-11-08 01:25:13 -0800

    final

--- lua/gen/init.lua
+++ lua/gen/init.lua
@@ -21,18 +21,12 @@ end
 
 local function get_window_options()
 
-    -- local width = math.floor(vim.o.columns * 0.9) -- 90% of the current editor's width
-    -- local height = math.floor(vim.o.lines * 0.9)
-
-    -- local row = math.floor((vim.o.lines - height) / 2)
-    -- local col = math.floor((vim.o.columns - width) / 2)
-
     local cursor = vim.api.nvim_win_get_cursor(0)
     local win_width = vim.api.nvim_win_get_width(0)
     local win_height = vim.api.nvim_win_get_height(0)
 
-    local new_win_height = math.floor(win_height * 0.8)
-    local new_win_width = math.floor(win_width * 0.8)
+    local new_win_height = math.floor(win_height-1)
+    local new_win_width = math.floor(win_width-2)
 
     local middle_row = win_height / 2
     local new_win_row
@@ -49,8 +43,7 @@ local function get_window_options()
         row = new_win_row,
         col = 0,
         style = 'minimal',
-        border = 'solid',
-        -- border = 'rounded',
+        border = 'none',
     }
 end
 
